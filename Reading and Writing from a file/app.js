6db9219422910564435905b86a86217ca407abdf
ajayygurjar <ajay1651@gmail.com>
2025-10-18 12:38:29 +0530

    Reading and Writing from a file

--- Reading and Writing from a file/app.js
+++ Reading and Writing from a file/app.js
@@ -1,5 +1,5 @@
 const http=require('http')
-
+const fs=require('fs')
 const server=http.createServer((req,res)=>{
     //url method
     const url=req.url;
@@ -22,17 +22,38 @@ const server=http.createServer((req,res)=>{
     }else{
         if(req.url==='/message'){
             res.setHeader('Content-type','text/html');
-            let dataChunk=[];
+            let body=[];
             req.on('data',(chunk)=>{
-                console.log(chunk)
-                dataChunk.push(chunk)
+                body.push(chunk)
             })
             req.on('end',()=>{
-                let combinedBuffer=Buffer.concat(dataChunk)
-                console.log(combinedBuffer.toString())
-                let value=combinedBuffer.toString().split('=')
-            console.log(value)
+                let buffer=Buffer.concat(body)
+                console.log(buffer)
+                let formData=buffer.toString();
+            console.log(formData)
+
+            const formValues=formData.split('=')[1];
+            fs.writeFile('formValues.txt',formValues,(err)=>{
+                res.statusCode=302;//redirect
+                res.setHeader('Location','/');
+                res.end();
             })
+
+            })
+        }
+        else{
+            if(req.url=='/read'){
+                //read from the file
+
+
+                fs.readFile('formValues.txt',(err,data)=>{
+                    console.log(data.toString())
+                    res.end(`
+                        <h1>${data.toString()}
+                        </h1>
+                        `);
+                })
+            }
         }
     }
 })
